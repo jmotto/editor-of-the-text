da3e3a156f7be88deb9851dcb873d40bc92128b5
Jamie Otto <jmotto31@gmail.com>
2022-10-10 17:53:01 -0700

    webpackpwamanifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,10 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -28,7 +31,22 @@ module.exports = () => {
       swDest: 'src-sw.js',
     }), 
       new WebpackPwaManifest({
-        
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
     ],
 
